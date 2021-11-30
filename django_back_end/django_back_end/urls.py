e8237b05b340e2392ce4685f425811ca7254b088
sang <phamcaosang135@gmail.com>
2021-11-30 10:20:49 +0700

    file change places

--- django_back_end/django_back_end/urls.py
+++ django_back_end/django_back_end/urls.py
@@ -1,7 +1,7 @@
-"""test_project URL Configuration
+"""django_back_end URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
