2e15cc77d1d38df5f88d000c559fab9ceaec7dc3
NguyenHoangPhuong <your_email@example.com>
2021-12-16 05:29:24 +0700

    update

--- django_back_end/django_back_end/urls.py
+++ django_back_end/django_back_end/urls.py
@@ -4,12 +4,12 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', admin.site.urls),
+    path('admin/', admin.site.urls),
     # path('admin/', admin.site.urls),
     path('api/v1/', include('djoser.urls')),
     path('api/v1/', include('djoser.urls.authtoken')),
     path('api/v1/', include('products.urls')),
     # path('api/v1/', include('order.urls')),
     # path('api/v1/', include('review.urls')),
-    path('api/v1/', include('user.urls'))
+    path('api/v1/users', include('user.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
